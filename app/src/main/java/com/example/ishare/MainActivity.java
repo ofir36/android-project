bd3425e757793e08c7f66d43bc61dc6a95cb1bd3
Unknown <ofir36@gmail.com>
2019-07-31 18:49:45 +0300

    added navigation and tab bar

--- app/src/main/java/com/example/ishare/MainActivity.java
+++ app/src/main/java/com/example/ishare/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.ishare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this,R.id.main_navhost_frag);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
